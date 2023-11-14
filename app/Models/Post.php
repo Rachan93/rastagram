1d699e56c29b2dc3332e0483fbcebaeca965a8c0
rachan93 <anas.rachafi_el_hausi@ifosup.wavre.be>
2023-11-14 15:29:34 +0100

    boulot 2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,8 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function commentaires()
+    {
+        return $this->hasMany(Commentaire::class);
+    }
 }

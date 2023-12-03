8b3b1def6c97bba394406fd35d1a47b57b86a245
rachan93 <anas.rachafi_el_hausi@ifosup.wavre.be>
2023-12-03 20:14:58 +0100

    comment+useless import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
         $this->call(LikeSeeder::class);
-        $this->call(FollowerSeeder::class);
+        $this->call(FollowerSeeder::class); // pour tester c'est plus facile de ne pas appeler ce seeder et de follow des gens manuellement, à défaut d'avoir une section reprenant l'ensemble des utilisateurs followés
 
     }
 }

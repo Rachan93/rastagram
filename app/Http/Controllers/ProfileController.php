a955a255d5277322d05ba704e0992ed6ec0ff80b
rachan93 <anas.rachafi_el_hausi@ifosup.wavre.be>
2023-12-03 19:58:10 +0100

    profilecontroller re-commented

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,40 +12,31 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-//profils publics
+    // Affiche les profils publics.
     public function show(User $user): View
     {
-        // Les articles publiés par l'utilisateur
+        // Récupérer les articles publiés par l'utilisateur avec le nombre de commentaires
         $posts = $user
             ->posts()
             ->withCount('comments')
             ->orderBy('created_at')
-            ->get()
-        ;
-    
-        // Les comments de l'utilisateur triés par date de création
+            ->get();
+
+        // Récupérer les commentaires de l'utilisateur triés par date de création
         $comments = $user
             ->comments()
             ->orderByDesc('created_at')
-            ->get()
-        ;
-        //dd($comments);
-    
-        // On renvoie la vue avec les données
+            ->get();
+
+        // Renvoyer la vue avec les données
         return view('profile.show', [
             'user' => $user,
             'posts' => $posts,
             'comments' => $comments,
         ]);
-      
     }
 
-
-
-
-    /**
-     * Display the user's profile form.
-     */
+    // Affiche le formulaire de modification du profil utilisateur.
     public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -53,50 +44,47 @@ class ProfileController extends Controller
         ]);
     }
 
-    /**
-     * Update the user's profile information.
-     */
+    // Met à jour les informations de profil de l'utilisateur.
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        //dd($request->all()); // Inspect the entire request
-    
+        // Remplir les données du modèle utilisateur avec les données validées du formulaire
         $request->user()->fill($request->validated());
         $request->user()->bio = $request->bio;
-    
-        //dd($request->user()); // Inspect the user model before saving
-    
+
+        // Réinitialiser la vérification de l'e-mail si l'adresse e-mail change
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-    
+
+        // Sauvegarder les modifications dans la base de données
         $request->user()->save();
-    
+
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
-    
+
+    // Met à jour l'avatar de l'utilisateur.
     public function updateAvatar(Request $request): RedirectResponse
     {
-       // dd($request);
-        // Validation de l'image sans passer par une form request
+        // Validation de l'image sans utiliser une form request
         $request->validate([
             'avatar' => ['required', 'image', 'max:2048'],
         ]);
- 
-        // Si l'image est valide, on la sauvegarde
+
+        // Si l'image est valide, la sauvegarder
         if ($request->hasFile('avatar')) {
             $user = $request->user();
             $path = $request->file('avatar')->store('avatars', 'public');
             $user->profile_photo = $path;
             $user->save();
         }
- 
+
         return Redirect::route('profile.edit')->with('status', 'avatar-updated');
     }
-    /**
-     * Delete the user's account.
-     */
+
+    // Supprime le compte utilisateur.
     public function destroy(Request $request): RedirectResponse
     {
+        // Validation du mot de passe actuel avant la suppression du compte
         $request->validateWithBag('userDeletion', [
             'password' => ['required', 'current_password'],
         ]);
@@ -107,15 +95,14 @@ class ProfileController extends Controller
 
         $user->delete();
 
+        // Invalidater la session et régénérer le jeton CSRF
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
     }
 
-
-
-
+    // Ajoute un utilisateur à la liste des abonnements.
     public function follow(User $user)
     {
         auth()->user()->following()->attach($user);
@@ -123,11 +110,11 @@ class ProfileController extends Controller
         return back();
     }
 
+    // Supprime un utilisateur de la liste des abonnements.
     public function unfollow(User $user)
     {
         auth()->user()->following()->detach($user);
 
         return back();
     }
-
 }
